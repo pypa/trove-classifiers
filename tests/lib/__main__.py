9b17f82e9e6779b6926e38f0d0a0ed7060a6acf8
Pradyun Gedam <pradyunsg@gmail.com>
2020-04-05 23:56:53 +0530

    Move tester -> tests.lib

--- tests/lib/__main__.py
+++ tests/lib/__main__.py
@@ -1,4 +1,4 @@
-from tester import trove_tester
+from tests.lib import trove_tester
 from trove_classifiers import classifiers, deprecated_classifiers
 
 trove_tester(classifiers, deprecated_classifiers)
