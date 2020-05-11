f2885b8603af4aa3c7bd5762be1de28dd13f87b2
Dustin Ingram <di@users.noreply.github.com>
2020-05-11 13:07:39 -0500

    Version 2020.05.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='trove-classifiers',
-    version='2020.04.09',
+    version='2020.05.11',
     description="Canonical source for classifiers on PyPI (pypi.org).",
     long_description=long_description,
     long_description_content_type="text/markdown",
