fd7a112bf917a8afffa70c821ff23ba5fee72999
Kevin Deldycke <kevin@deldycke.com>
2020-09-03 17:34:26 +0200

    Sorting is case-sensitive.

--- src/trove_classifiers/__init__.py
+++ src/trove_classifiers/__init__.py
@@ -729,10 +729,10 @@ classifiers = {
     "Topic :: Text Processing :: Markup :: HTML",
     "Topic :: Text Processing :: Markup :: LaTeX",
     "Topic :: Text Processing :: Markup :: Markdown",
-    "Topic :: Text Processing :: Markup :: reStructuredText",
     "Topic :: Text Processing :: Markup :: SGML",
     "Topic :: Text Processing :: Markup :: VRML",
     "Topic :: Text Processing :: Markup :: XML",
+    "Topic :: Text Processing :: Markup :: reStructuredText",
     "Topic :: Utilities",
     "Typing :: Typed",
 }
