2fca3e366fa8814539dbdd76511bc297bda7f15a
John (EBo) David <ebo@users.noreply.github.com>
2024-03-03 15:16:34 -0500

    add  "License :: OSI Approved :: NASA Open Source Agreement (NOSA)" (#167)
    * added NOSA
    
    * Update src/trove_classifiers/__init__.py
    
    ---------
    
    Co-authored-by: John (EBo) David <ebo@swcp.com>
    Co-authored-by: Dustin Ingram <di@users.noreply.github.com>

--- src/trove_classifiers/__init__.py
+++ src/trove_classifiers/__init__.py
@@ -312,6 +312,7 @@ sorted_classifiers: List[str] = [
     "License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)",
     "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
     "License :: OSI Approved :: Mulan Permissive Software License v2 (MulanPSL-2.0)",
+    "License :: OSI Approved :: NASA Open Source Agreement v1.3 (NASA-1.3)",
     "License :: OSI Approved :: Nethack General Public License",
     "License :: OSI Approved :: Nokia Open Source License",
     "License :: OSI Approved :: Open Group Test Suite License",
