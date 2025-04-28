becb053821dad4b9e6d37392754b6b5a830be596
Randall Pittman <162228805+RandallPittmanOrSt@users.noreply.github.com>
2025-04-28 15:48:50 -0700

    Add entry point to enable installing with pipx or uvx (#211)
    * Add entry point to enable installing with pipx or uvx
    
    * Tests added for running the module and calling the entry point.
    
    * Add return type annotation
    
    * reformat
    
    * Try adding BINDIR to python and entry point
    
    * Add missing BINDIR paths
    
    * Add docstring to test.

--- src/trove_classifiers/__main__.py
+++ src/trove_classifiers/__main__.py
@@ -1,4 +1,10 @@
 from trove_classifiers import sorted_classifiers
 
-for classifier in sorted_classifiers:
-    print(classifier)
+
+def cli() -> None:
+    for classifier in sorted_classifiers:
+        print(classifier)
+
+
+if __name__ == "__main__":
+    cli()
