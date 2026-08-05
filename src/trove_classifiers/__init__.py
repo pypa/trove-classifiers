d4ef96a3e7e04dd1d412ffde8fc06009925e3a7c
Lanre Shittu <136805224+Shizoqua@users.noreply.github.com>
2026-08-05 02:13:39 +0100

    Add Framework :: SageMath classifier
    Signed-off-by: Lanre Shittu <136805224+Shizoqua@users.noreply.github.com>

--- src/trove_classifiers/__init__.py
+++ src/trove_classifiers/__init__.py
@@ -234,6 +234,7 @@ sorted_classifiers: List[str] = [
     "Framework :: Robot Framework",
     "Framework :: Robot Framework :: Library",
     "Framework :: Robot Framework :: Tool",
+    "Framework :: SageMath",
     "Framework :: Scrapy",
     "Framework :: Setuptools Plugin",
     "Framework :: Sphinx",
